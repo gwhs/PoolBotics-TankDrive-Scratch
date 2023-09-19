92b0041d6cf5f2256c31d035328afc45602ff132
Chvoong24 <89755355+Chvoong24@users.noreply.github.com>
2023-09-19 00:08:52 -0700

    added work

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,4 +16,14 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
+  public static class IDConstants {
+    public static final int BackRightSteerID = 1;
+    public static final int BackRightDriveID = 2;
+    public static final int BackLeftSteerID = 3;
+    public static final int BackLeftDriveId = 4;
+    public static final int FrontRightSteerID = 5;
+    public static final int FrontLeftDriveID = 6;
+    public static final int FrontLeftSteerID = 7;
+    public static final int FrontRightDriveID = 8;
+  }
 }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,7 +7,9 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-
+import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.Constants.IDConstants;
+import frc.robot.Constants.OperatorConstants;
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
@@ -15,9 +17,13 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  * project.
  */
 public class Robot extends TimedRobot {
+
+
   private Command m_autonomousCommand;
 
+
   private RobotContainer m_robotContainer;
+  private final XboxController m_XboxController = new XboxController(OperatorConstants.kDriverControllerPort);
 
   /**
    * This function is run when the robot is first started up and should be used for any
